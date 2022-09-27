59721d793f2226b004b8bee09b4dd05164bdc150
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-27 23:26:36 +0330

    basic math

---
+++ Introduction/basic math/basic math1.r
@@ -0,0 +1,25 @@
+x <- 11
+y <- 4
+
+#addition
+print(x+y)
+
+#substraction
+print(x-y)
+
+#multiplication
+print(x*y)
+
+#division
+print(x/y)
+
+#exponentation
+print(x^y) #or x**y
+
+print(x**y)
+
+#modulus (remainder from division)
+print(x%%y)
+
+#integer division
+print(x%/%y) 
---
+++ Introduction/basic math/basic math2.r
@@ -0,0 +1,15 @@
+x <- 12
+y <- 5
+
+#maximum
+print(max(x, y))
+
+
+#minimum
+print(min(x, y))
+
+#You can also use more than 2 numbers with the min and max functions - just separate them using commas.
+
+# square root 
+
+print(sqrt(64))
