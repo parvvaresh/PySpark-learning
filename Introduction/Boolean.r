538e6fe6f50e5da65fbb0581a83eb4d7766898fc
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-27 23:50:14 +0330

    Boolean

---
+++ Introduction/Boolean.r
@@ -0,0 +1,21 @@
+#Boolean is another data type in R.
+#It can have one of the following 2 values: TRUE and FALSE.
+
+#Booleans are created when we compare values.
+
+x <- 42
+
+print(x > 10)
+
+#R supports the following relational operators, used for comparisons:
+#  > greater than;
+#< less than;
+#<= less than or equal to;
+#>= greater than or equal to;
+#== equal
+#!= not equal
+
+print(x >= 8)
+print(x < 24)
+print(x == 42)
+print(x != 42)
