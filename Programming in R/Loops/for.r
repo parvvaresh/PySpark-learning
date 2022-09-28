ef9c344c1b82d7bd8cce48a4f16e89e41866bb6b
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-28 19:55:19 +0330

    Loops

---
+++ Programming in R/Loops/for.r
@@ -0,0 +1,7 @@
+#R allows us to create a sequence of numbers by using a colon and specifying the lower and upper bounds. The sequence in the code above will include the numbers 1 to 10.
+#During each iteration of the for loop, the x variable will take the value of the next number in the sequence, thus, the resulting output will be the numbers 1 to 10.
+
+for(x in 1:10){
+  print(x)
+}
+
---
+++ Programming in R/Loops/while.r
@@ -0,0 +1,11 @@
+
+
+
+#Loops allow you to repeat a block of code until a given condition is TRUE
+
+i <- 1
+
+while(i <= 6){
+  print(i)
+  i <- i+1
+}
