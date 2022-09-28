ecc90986dac249749bc3fe38c6372828915181be
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-28 18:42:27 +0330

    Decision Making

---
+++ Decision Making/if.r
@@ -0,0 +1,12 @@
+#In many situations, you need to make a decision based on a condition.
+#For that, you can use the if statement.
+
+#As you can see, the if keyword is followed by the condition in parentheses 
+#and a code block in curly braces, which gets executed if the condition is TRUE.
+
+
+x <- 42
+
+if(x > 50){
+  print("this number geater than 50")
+}
