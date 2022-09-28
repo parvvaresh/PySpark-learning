a6944f6e34e14238086099b924822537f4dedf08
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-09-28 17:40:15 +0330

    Decision Making

---
+++ Decision Making/else if.r
@@ -0,0 +1,14 @@
+#In case you need multiple checks, you can use multiple else if statements.
+#For example, let's output the English version of the given number:
+
+
+num <- 3
+if(num == 1) {
+  print("One")
+} else if(num == 2) {
+  print("Two")
+} else if (num == 3) {
+  print("Three")
+} else {
+  print("Something else")
+}
---
+++ Decision Making/else.r
@@ -0,0 +1,9 @@
+#In case you need to run code when the condition of an if statement is FALSE, you can use an else statement
+
+x <- 42
+
+if(x < 30){
+  print("yes")
+}else{
+  print("no")
+}
